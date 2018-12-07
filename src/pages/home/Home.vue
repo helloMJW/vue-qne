d2bd95387b0929ce5df0a9239497a58c4feb789a
辣条拌鱼翅 <majianwei168@outlook.com>
2018-12-07 10:40:39 +0800

    头部搜索区域

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
-        <h1> Hello World, this is home</h1>
+        <home-header></home-header>
     </div>
 </template>
 
 <script>
-
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
