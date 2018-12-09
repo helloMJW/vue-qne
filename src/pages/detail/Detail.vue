eb88845f3ae212b1214ef68ae8fca6fcb8af16c1
辣条拌鱼翅 <majianwei168@outlook.com>
2018-12-09 14:58:29 +0800

    递归组件实现

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,18 +2,43 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人成人成人'
+          }]
+        }, {
+          title: '成人两馆联票'
+        }]
+      }, {
+        title: '学生篇'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
