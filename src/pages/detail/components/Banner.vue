0a505c5fbd4e43793a8088b05cac7114e44a6f8f
辣条拌鱼翅 <majianwei168@outlook.com>
2018-12-09 11:31:27 +0800

    iconfont更新时需要替换base64一段代码 background-image可以写出渐变效果

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1502/15/15ca1d0eab6bf46c.water.jpg_600x330_1108fbd3.jpg" alt="">
+    <div class="banner-info">
+      <div class="banner-tittle">东部华侨城大侠谷</div>
+      <div class="banner-number">
+        <span class="iconfont banner-icon">&#xe692;</span>
+        39
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-tittle
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        padding: 0 .4rem
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
